0b912f855d2e1fba34f4f835dc1dd577bc18b502
AnufrikovaViktoria <Phoenix-Viki@yandex.ru>
2024-01-15 12:35:03 +0700

    First

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE =  "https://ad8c07c4-501a-41c9-a1c8-f9a218982e47.serverhub.praktikum-services.ru"
+CREATE_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+        "firstName": "Виктория",
+        "lastName": "Ануфрикова",
+        "address": "г. Новосибирск, ул. Петухова, д. 17",
+        "metroStation": 4,
+        "phone": "+7 913 355 35 35",
+        "rentTime": 5,
+        "deliveryDate": "2024-01-20",
+        "comment": "Улыбайтесь!",
+        "color": ["BLACK"]
+    }
